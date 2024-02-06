41f1039c9e3c193af6351da6816f1ed2c98e42d1
Layne <neonisdaboi@outlook.com>
2024-02-06 01:45:02 -0600

    Update RemoveEdge.bat

--- RemoveEdge.bat
+++ RemoveEdge.bat
@@ -1,6 +1,18 @@
-taskkill /f /im msedge.exe
-taskkill /f /im msedgewebview2.exe
-@RD /S /Q "C:\Program Files (x86)\Microsoft\Edge"
-@RD /S /Q "C:\Program Files (x86)\Microsoft\EdgeCore"
-@RD /S /Q "C:\Program Files (x86)\Microsoft\EdgeUpdate"
-@RD /S /Q "C:\Program Files (x86)\Microsoft\Temp"
+@echo off
+goto check_Permissions
+
+:check_Permissions
+    net session >nul 2>&1
+    if %errorLevel% == 0 (
+        taskkill /f /im msedge.exe
+	taskkill /f /im msedgewebview2.exe
+	@RD /S /Q "C:\Program Files (x86)\Microsoft\Edge"
+	@RD /S /Q "C:\Program Files (x86)\Microsoft\EdgeCore"
+	@RD /S /Q "C:\Program Files (x86)\Microsoft\EdgeUpdate"
+	@RD /S /Q "C:\Program Files (x86)\Microsoft\Temp"
+	echo Microsoft Edge should be gone by now...
+    ) else (
+        echo Please Run Again As Administrator!
+    )
+    
+    pause >nul
